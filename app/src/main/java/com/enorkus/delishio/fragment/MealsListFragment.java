9d90854c230d2a7199c6bb2a20ed8700721147c2
enorkus <eitvys@gmail.com>
2018-07-19 18:55:18 +0300

    More simple way of adding new row for new ingredient

--- app/src/main/java/com/enorkus/delishio/fragment/MealsListFragment.java
+++ app/src/main/java/com/enorkus/delishio/fragment/MealsListFragment.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.enorkus.delishio.R;
 import com.enorkus.delishio.activity.AddMealActivity;
@@ -48,6 +47,7 @@ public class MealsListFragment extends Fragment {
         RecyclerView.Adapter adapter = new MealsListAdapter(meals);
         mealsListRecyclerView.setAdapter(adapter);
 
+        fab.setImageResource(R.drawable.ic_remove_white_24dp);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/enorkus/delishio/listener/AddMealIngredientClickListener.java
+++ app/src/main/java/com/enorkus/delishio/listener/AddMealIngredientClickListener.java
@@ -1,10 +1,6 @@
 package com.enorkus.delishio.listener;
 
-import android.os.Build;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TextInputEditText;
-import android.support.design.widget.TextInputLayout;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -23,40 +19,9 @@ public class AddMealIngredientClickListener implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        LinearLayout ingredientLinearLayout = new LinearLayout(ctx);
-        TextInputLayout ingredientInputLayout = new TextInputLayout(ctx);
-        TextInputEditText ingredientEditText = new TextInputEditText (ctx);
-        FloatingActionButton removeIngredientBtn = new FloatingActionButton(ctx);
-
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        LinearLayout.LayoutParams fabLayoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 3.0f);
-
-        removeIngredientBtn.setId(View.generateViewId());
-        removeIngredientBtn.setLayoutParams(fabLayoutParams);
-        removeIngredientBtn.setClickable(true);
-        removeIngredientBtn.setFocusable(true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            removeIngredientBtn.setElevation(0);
-        }
-        removeIngredientBtn.setImageDrawable(ctx.getResources().getDrawable(R.drawable.ic_remove_black_24dp));
-
-        ingredientEditText.setLayoutParams(layoutParams);
-        ingredientLinearLayout.setLayoutParams(layoutParams);
-        ingredientLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-        layoutParams.weight = 7.0f;
-        ingredientInputLayout.setLayoutParams(layoutParams);
-        ingredientInputLayout.setHint(ctx.getResources().getString(R.string.meal_ingredient));
-
-        ingredientInputLayout.addView(ingredientEditText);
-        ingredientLinearLayout.addView(ingredientInputLayout, layoutParams);
-        ingredientLinearLayout.addView(removeIngredientBtn, layoutParams);
-        //This only works after fab is added to the view.
-        ((LinearLayout.LayoutParams)removeIngredientBtn.getLayoutParams()).gravity = Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL;
-        ingredientsLayout.addView(ingredientLinearLayout, layoutParams);
-
+        LinearLayout layout = ctx.findViewById(R.id.ingredientsLinearLayout);
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View newRow  = inflater.inflate(R.layout.add_ingredient_row, layout, false);
+        layout.addView(newRow);
     }
 }
