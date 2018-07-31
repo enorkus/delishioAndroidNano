dab71a5438d795a59a98662eb9af925af9dec05d
enorkus <eitvys@gmail.com>
2018-07-31 20:28:33 +0300

    Moved pixabay API key to gradle.properties

--- app/src/main/java/com/enorkus/delishio/util/ConnectionUtils.java
+++ app/src/main/java/com/enorkus/delishio/util/ConnectionUtils.java
@@ -2,19 +2,20 @@ package com.enorkus.delishio.util;
 
 import android.net.Uri;
 
+import com.enorkus.delishio.BuildConfig;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ConnectionUtils {
 
     private static final String PIXABAY_BASE_URL = "https://pixabay.com/api/";
-    private static final String API_KEY = "";
     private static final String PARAM_KEY = "key";
     private static final String PARAM_QUERY = "q";
 
     public static URL buildImageSearchURL(String searchParam) {
         Uri uri = Uri.parse(PIXABAY_BASE_URL).buildUpon()
-                .appendQueryParameter(PARAM_KEY, API_KEY)
+                .appendQueryParameter(PARAM_KEY, BuildConfig.PixabayAPIKey)
                 .appendQueryParameter(PARAM_QUERY, searchParam)
                 .build();
         try {
